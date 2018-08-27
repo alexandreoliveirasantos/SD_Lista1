0140419ac48d4757e62d0ef48bbddc2a6ded2dd0
Alexandre Oliveira <alexandre121996@hotmail.com>
2018-08-27 17:11:30 -0300

    Introdução Views
    Introdução do mecanismo de Views através do mecanismo Pug Template Engine e melhorias gerais.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express') /*Chamar o express*/
 const server = express() /*Criando o server (createserver do HTTP)*/
+server.set('view engine', 'pug')
 
 var bodyParser = require('body-parser')
 server.use(bodyParser.urlencoded({
@@ -7,24 +8,13 @@ server.use(bodyParser.urlencoded({
 }))
 
 server.get('/', (req, res) => {
-	res.send(`
-			<h1 align="center">Primeira lista de Sistemas Distribuídos - 2018-2</h1>
-			<div align="center">
-				<a href="questao1"> Questao 1 </a> <br>
-				<a href="questao2"> Questao 2 </a> <br>
-				<a href="questao3"> Questao 3 </a> <br>
-				<a href="questao4"> Questao 4 </a> <br>
-				<a href="questao5"> Questao 5 </a> <br>
-				<a href="questao6"> Questao 6 </a> <br>
-				<a href="questao7"> Questao 7 </a> <br>
-				<a href="questao8"> Questao 8 </a> <br>
-				<!--<a href="questao9"> Questao 9 </a> <br>-->
-			<div>
-		`)
+	res.render('index', {})
+	/*Ex caso fosse passar valores para view > res.render('questao1', { title: 'Hey', message: 'Hello there!'});*/
 })
 
 server.get('/questao1', (req, res) => {
-	res.send(`
+	res.render('questao1', {})
+	/*res.send(`
 			<h1>Questao #1: Reajuste de Salários</h1>
 			<div align="center">
 				<form action ="/reajusta" method="POST">
@@ -45,7 +35,7 @@ server.get('/questao1', (req, res) => {
 				<p>Cargo operador: Reajuste de 20%</p>
 				<p>Cargo programador: Reajuste de 18%</p>
 			<div>
-		`)
+		`)*/
 })
 
 server.post('/reajusta', (req, res)=>{
@@ -69,29 +59,7 @@ server.post('/reajusta', (req, res)=>{
 
 
 server.get('/questao2', (req, res) => {
-	res.send(`
-			<h1>Questao #2: Verifica maioridade</h1>
-			<div align="center">
-				<form action ="/maiorMenor" method="POST">
-				<p> Digite a seguir o seu nome, sexo e idade: </p>
-					<label for="nome"> Nome:</label>
-					<input type="nome" name="nome" id="nome">
-					<br>
-					<label for="sexo"> Sexo:</label>
-					<input type="sexo" name="sexo" id="sexo">
-					<br>
-					<label for="idade"> Idade:</label>
-					<input type="idade" name="idade" id="idade">
-					<br>
-					<br><br>
-					<input type="submit" value="Submeter">
-				</form>
-				<br>
-				<h2> Valores de referencia:</h2>
-				<p>Sexo masculino: Maioridade a partir de 18 anos</p>
-				<p>Sexo feminino: Maioridade a partir de 21 anos</p>
-			</div>
-		`)
+	res.render('questao2', {})
 })
 
 server.post('/maiorMenor', (req, res)=>{
@@ -119,31 +87,7 @@ server.post('/maiorMenor', (req, res)=>{
 
 
 server.get('/questao3', (req, res) => {
-	res.send(`
-			<h1>Questao #3: Resultado - Notas</h1>
-			<div align="center">
-				<form action ="/resultadoNotas" method="POST">
-				<p> Insira a seguir as notas N1, N2 e N3 (0 a 10): </p>
-					<label for="nota1"> N1:</label>
-					<input type="nota1" name="nota1" id="nota1">
-					<br>
-					<label for="nota2"> N2:</label>
-					<input type="nota2" name="nota2" id="nota2">
-					<br>
-					<label for="nota3"> N3:</label>
-					<input type="nota3" name="nota3" id="nota3">
-					<br>
-					<br><br>
-					<input type="submit" value="Submeter">
-				</form>
-				<br>
-				<h2> Valores de referencia:</h2>
-				<p>Media aritmetica entre N1 e N2 >= 7.0: Aprovado(a)!</p>
-				<p>Media aritmetica entre N1 e N2 >= 3.0 e menor que 7.0: Estudante devera realizar N3!</p>
-				<p>Media aritmetica (N1 e N2) + N3 / 2 >= 5.0: Aprovado(a)</p>
-				<p>Demais casos: Reprovado(a)</p>
-			</div>
-		`)
+	res.render('questao3', {})
 })
 
 server.post('/resultadoNotas', (req, res)=>{
@@ -171,45 +115,23 @@ server.post('/resultadoNotas', (req, res)=>{
 
 
 server.get('/questao4', (req, res) => {
-	res.send(`
-			<h1>Questao #4: Peso Ideal</h1>
-			<div align="center">
-				<form action ="/pesoIdeal" method="POST">
-				<p> Insira a seguir sua altura e sexo (masculino ou feminino): </p>
-					<label for="altura"> Altura:</label>
-					<input type="altura" name="altura" id="altura">
-					<br>
-					<label for="sexo"> Sexo:</label>
-					<input type="sexo" name="sexo" id="sexo">
-					<br><br>
-					<input type="submit" value="Calcular peso Ideal">
-				</form>
-				<br>
-				<h2> Valores de referencia:</h2>
-				<p>Sexo masculino: (72.7 * altura) -58</p>
-				<p>Sexo feminino:  (62.1 * altura) -44.7</p>
-			</div>
-		`)
+	res.render('questao4', {})
 })
 
 server.post('/pesoIdeal', (req, res)=>{
 	var altura = parseFloat(req.body.altura)
 	var sexo = req.body.sexo
-
 	var peso = null
 
 	switch(sexo){
-
 		case "masculino":
 			peso = ((72.7 * altura) - 58)
 			res.send('<h1>Sexo: '+sexo +' | Altura: ' +altura +'<br> Peso ideal: ' +peso +'.</h1>')
 		break
-
 		case "feminino":
 			peso = ((62.1 * altura) - 44.7)
 			res.send('<h1>Sexo: '+sexo +' | Altura: ' +altura +'<br> Peso ideal: ' +peso +'.</h1>')
 		break
-
 		default:
 			res.send('<h1>Verifique os dados inseridos e tente novamente!</h1>')
 	}
@@ -217,25 +139,7 @@ server.post('/pesoIdeal', (req, res)=>{
 
 
 server.get('/questao5', (req, res) => {
-	res.send(`
-			<h1>Questao #5: Categoria nadador</h1>
-			<div align="center">
-				<form action ="/categoriaNadador" method="POST">
-					<p> Insira a seguir sua idade: </p>
-					<label for="idade"> Idade:</label>
-					<input type="idade" name="idade" id="idade">
-					<br><br>
-					<input type="submit" value="Verificar categoria">
-				</form>
-				<br>
-				<h2> Valores de referencia:</h2>
-				<p>Categoria Infantil A: 5  -  7 anos</p>
-				<p>Categoria Infantil B: 8  - 10 anos</p>
-				<p>Categoria Juvenil  A: 11 - 13 anos</p>
-				<p>Categoria Juvenil  B: 14 - 17 anos</p>
-				<p>Categoria Adulto    : Maiores de 18 anos</p>
-			</div>
-		`)
+	res.render('questao5', {})
 })
 
 server.post('/categoriaNadador', (req, res)=>{
@@ -258,30 +162,7 @@ server.post('/categoriaNadador', (req, res)=>{
 
 
 server.get('/questao6', (req, res) => {
-	res.send(`
-			<h1>Questao #6: Calculo Salario Liquido</h1>
-			<div align="center">
-				<form action ="/salarioLiquido" method="POST">
-					<p> Insira a seguir o nome, nivel (A,B,C,D), salario bruto e nº de dependentes do funcionario: </p>
-					<label for="nome"> Nome:</label>
-					<input type="nome" name="nome" id="nome"><br>
-					<label for="nivel"> Nivel:</label>
-					<input type="nivel" name="nivel" id="nivel"><br>
-					<label for="salBruto"> Salario Bruto:</label>
-					<input type="salBruto" name="salBruto" id="salBruto"><br>
-					<label for="numDepend"> Nº dependentes:</label>
-					<input type="numDepend" name="numDepend" id="numDepend"><br>
-					<br><br>
-					<input type="submit" value="Submeter">
-				</form>
-				<br>
-				<h2> Valores de referencia:</h2>
-				<p>Nivel A: Desconto de 3% caso não houver dependentes e  8% caso houver</p>
-				<p>Nivel B: Desconto de 5% caso não houver dependentes e  10% caso houver</p>
-				<p>Nivel C: Desconto de 8% caso não houver dependentes e  15% caso houver</p>
-				<p>Nivel D: Desconto de 10% caso não houver dependentes e 17% caso houver</p>
-			</div>
-		`)
+	res.render('questao6', {})
 })
 
 server.post('/salarioLiquido', (req, res)=>{
@@ -335,25 +216,7 @@ server.post('/salarioLiquido', (req, res)=>{
 
 
 server.get('/questao7', (req, res) => {
-	res.send(`
-			<h1>Questao #7: Verifica Aposentadoria</h1>
-			<div align="center">
-				<form action ="/verificaAposentadoria" method="POST">
-					<p> Insira a seguir a idade, sexo (masculino ou feminino) e tempo de servico do funcionario: </p>
-					<label for="idade"> Idade:</label>
-					<input type="idade" name="idade" id="idade"><br>
-					<label for="sexo"> Sexo:</label>
-					<input type="sexo" name="sexo" id="sexo"><br>
-					<label for="tempoServ"> Tempo servico:</label>
-					<input type="tempoServ" name="tempoServ" id="tempoServ"> <br><br>
-					<input type="submit" value="Submeter">
-				</form>
-				<br>
-				<h2> Valores de referencia:</h2>
-				<p>Sexo masculino: Minimo 65 anos de idade e ter trabalhado por no minimo 30 anos</p>
-				<p>Sexo feminino: Minimo 60 anos de idade e ter trabalhado por no minimo 25 anos</p>
-			</div>
-		`)
+	res.render('questao7', {})
 })
 
 server.post('/verificaAposentadoria', (req, res)=>{
@@ -385,24 +248,7 @@ server.post('/verificaAposentadoria', (req, res)=>{
 
 
 server.get('/questao8', (req, res) => {
-	res.send(`
-			<h1>Questao #8: Concessao de Credito</h1>
-			<div align="center">
-				<form action ="/concedeCredito" method="POST">
-					<p> Insira a seguir o saldo medio do cliente no ano anterior: </p>
-					<label for="saldoMedio"> Saldo Medio:</label>
-					<input type="saldoMedio" name="saldoMedio" id="saldoMedio"><br>
-					<br><br>
-					<input type="submit" value="Submeter">
-				</form>
-				<br>
-				<h2> Valores de referencia:</h2>
-				<p>Saldo Medio de 0 a 200: Nenhum Credito</p>
-				<p>Saldo Medio de 201 a 400: 20% do valor do saldo medio</p>
-				<p>Saldo Medio de 401 a 600: 30% do valor do saldo medioo</p>
-				<p>Saldo Medio acima de 601: 40% do valor do saldo medio</p>
-			</div>
-		`)
+	res.render('questao8', {})
 })
 
 server.post('/concedeCredito', (req, res)=>{
